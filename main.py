f45bad594683ecbdb3b920294462908af02cb8d3
PacMan2009yo <41653862+PacMan2009yo@users.noreply.github.com>
2021-03-13 17:03:59 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube
-
-url = input("Paste the url of any YouTube video here\n")
-video = YouTube(url)
-
-print("Downloading...")
-video.streams.get_highest_resolution().download("Video")
-print("Video downloaded.")
